484892383d7a158b2818da7f3d4628fb841675ff
bobscott78 <bobscott78@gmail.com>
2014-03-24 22:41:34 +0000

    Initial commit

---
+++ BowlingKata/BowlingTests.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Linq;
+using NUnit.Framework;
+
+namespace BowlingKata
+{
+    [TestFixture]
+    public class BowlingTests
+    {
+        [Test]
+        public void NormalFrame()
+        {
+            var frames = "2,3".Split(',').Select(int.Parse).ToObservable();
+            
+            var game = new Game(frames);
+
+            Assert.That(game.Score(), Is.EqualTo(5));
+        }
+
+        [Test]
+        public void SpareFrameScoresNextDouble()
+        {
+            var frames = "9,1,7,0".Split(',').Select(int.Parse).ToObservable();
+
+            var game = new Game(frames);
+
+            Assert.That(game.Score(), Is.EqualTo(24));
+        }
+
+        [Test]
+        public void TwoSparesFrameScoresNextDouble()
+        {
+            var frames = "9,1,7,3,2,0".Split(',').Select(int.Parse).ToObservable();
+
+            var game = new Game(frames);
+
+            Assert.That(game.Score(), Is.EqualTo(31));
+        }
+
+        [Test]
+        public void StrikeFrameScoresNextTwoDouble()
+        {
+            var frames = "10,7,3".Split(',').Select(int.Parse).ToObservable();
+
+            var game = new Game(frames);
+
+            Assert.That(game.Score(), Is.EqualTo(30));
+        }
+
+        [Test]
+        public void TwoStrikeFrameScoresNextTwoDouble()
+        {
+            var frames = "10,10,7,3".Split(',').Select(int.Parse).ToObservable();
+
+            var game = new Game(frames);
+
+            Assert.That(game.Score(), Is.EqualTo(57));
+        }
+
+        [Test]
+        public void PerfectGame()
+        {
+            var frames = "10,10,10,10,10,10,10,10,10,10,10,10".Split(',').Select(int.Parse).ToObservable();
+
+            var game = new Game(frames);
+
+            Assert.That(game.Score(), Is.EqualTo(300));
+        }
+    }
+
+    public class Game
+    {
+        private readonly IObservable<int> _score;
+        private int _frameScore;
+        private bool _firstBall = true;
+        private int _nextBallCoefficient = 1;
+        private int _ballAfterNextCoefficient = 1;
+        private int _frameCount;
+
+        public Game(IObservable<int> frames)
+        {
+            _score = frames.Scan(0, Accumulator);
+        }
+
+        private int Accumulator(int acc, int current)
+        {
+            _frameScore += current;
+            
+            var score = acc + (_nextBallCoefficient * current);
+            _nextBallCoefficient = _ballAfterNextCoefficient;
+            _ballAfterNextCoefficient = 1;
+
+            var isStrike = IsStrike();
+            var isSpare = IsSpare();
+            var isFrame = IsFrame();
+
+            if (isStrike)
+            {
+                _nextBallCoefficient++;
+                _ballAfterNextCoefficient++;
+                _firstBall = true;
+                _frameScore = 0;
+                _frameCount++;
+            }
+            else if (isSpare)
+            {
+                _nextBallCoefficient++;
+                _frameScore = 0;
+                _firstBall = true;
+                _frameCount++;
+            }
+            else if (isFrame)
+            {
+                _frameScore = 0;
+                _firstBall = true;
+                _frameCount++;
+            }
+            else
+            {
+                _firstBall = false;
+            }
+            return score;
+        }
+
+        private bool IsFrame()
+        {
+            return !_firstBall && !IsLastFrame();
+        }
+
+        private bool IsSpare()
+        {
+            return !_firstBall && (_frameScore == 10) && !IsLastFrame();
+        }
+
+        private bool IsStrike()
+        {
+            return _firstBall && (_frameScore == 10) && !IsLastFrame();
+        }
+
+        private bool IsLastFrame()
+        {
+            return (_frameCount >= 9);
+        }
+
+        public int Score()
+        {
+            return _score.Last();
+        }
+    }
+}
---
+++ GameController/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using BowlingKata;
+
+namespace GameController
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var finished = false;
+            var scores = Observable.Create<int>(o =>
+                {
+                    while (!finished)
+                    {
+                        var readLine = Console.ReadLine();
+                        if (readLine == "END")
+                        {
+                            finished = true;
+                        }
+                        else
+                        {
+                            o.OnNext(int.Parse(readLine));
+                        }
+                    }
+                    o.OnCompleted();
+                    return Disposable.Empty;
+                });
+
+            var game = new Game(scores);
+            Console.WriteLine(game.Score());
+            Console.ReadLine();
+        }
+    }
+}
